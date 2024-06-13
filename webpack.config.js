49f13ba3c46ae2df039aa6c95dec90b4d0265500
Runas24 <runnnasss4@gmail.com>
2024-06-13 19:45:41 +0500

    654645

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function(env, argv) {
+  if (!env.mode) {
+    throw new Error('Mode not specified in the environment');
+  }
+
   const config = await createExpoWebpackConfigAsync(env, argv);
   // Customize the config here if needed
   return config;
